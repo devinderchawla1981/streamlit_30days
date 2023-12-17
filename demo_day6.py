f7f59ab8dea6a74a6c09465cd7ddfb9fa2786c7b
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-12-17 18:54:34 +0000

    Update demo_day6.py

--- demo_day6.py
+++ demo_day6.py
@@ -7,14 +7,14 @@ df = pd.DataFrame(
     np.random.randn(200, 3),
     columns=['a', 'b', 'c'])
 
-It looks like you've created a Pandas DataFrame named df with 200 rows and 3 columns ('a', 'b', 'c'), filled with random values from a standard normal distribution (using np.random.randn). This is a common way to create a DataFrame for testing or demonstration purposes in data analysis with Python and Pandas.
+#It looks like you've created a Pandas DataFrame named df with 200 rows and 3 columns ('a', 'b', 'c'), filled with random values from a standard normal distribution (using np.random.randn). This is a common way to create a DataFrame for testing or demonstration purposes in data analysis with Python and Pandas.
 
 c = alt.Chart(df).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
-It appears that you're using the Altair library to create a scatter plot (mark_circle()) based on the data in your Pandas DataFrame df. The x-axis is represented by the 'a' column, the y-axis by the 'b' column, and the size and color of the circles are determined by the 'c' column. Additionally, you've specified a tooltip to display information about the 'a', 'b', and 'c' values when hovering over data points.
+#It appears that you're using the Altair library to create a scatter plot (mark_circle()) based on the data in your Pandas DataFrame df. The x-axis is represented by the 'a' column, the y-axis by the 'b' column, and the size and color of the circles are determined by the 'c' column. Additionally, you've specified a tooltip to display information about the 'a', 'b', and 'c' values when hovering over data points.
 
-Here's a breakdown of the Altair chart code:
+#Here's a breakdown of the Altair chart code:
 
 alt.Chart(df): Specifies the DataFrame (df) as the data source for the chart.
 .mark_circle(): Specifies that you want to create a scatter plot with circles.
