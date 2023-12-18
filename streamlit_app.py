52858511638f46115c8512c6998b1d194572d169
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-12-18 17:52:54 +0000

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,94 @@
+""" How to put multiline comments 
+select lines and press shift+Alt+A
+"""
+
+
+import streamlit as st
+from datetime import time, datetime
+
+st.title("My First App")
+# st.write('Hello London world in Illford')
+""" 
+st_name=st.sidebar.text_input("Enter your name")
+st.write(f'Welcome  {st_name}!' )
+
+st.header("st.button")
+
+#Day 4
+if st.button("Say Hello"):
+    st.write('why Hello there')
+else:
+    st.write("Goodbye")
+ """
+#Day 8
+    
+# st.header('st.slider')
+
+# st.subheader('Slider')
+
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write("I'm ", age, 'years old')
+
+# st.subheader('Range slider')
+
+# values = st.slider(
+#      'Select a range of values',
+#      0.0, 100.0, (25.0, 75.0))
+# st.write('Values:', values)
+
+# st.subheader('Range time slider')
+
+# appointment = st.slider(
+#      "Schedule your appointment:",
+#      value=(time(11, 30), time(12, 45)))
+# st.write("You're scheduled for:", appointment)
+
+# st.subheader('Datetime slider')
+
+# start_time = st.slider(
+#      "When do you start?",
+#      value=datetime(2020, 1, 1, 9, 30),
+#      format="MM/DD/YY - hh:mm")
+# st.write("Start time:", start_time)
+
+""" import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+ """
+ 
+
+import streamlit as st
+import pandas as pd
+
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
+
+
+""" import streamlit as st
+
+st.set_page_config(layout="wide") """
+
+
+
+
+
+
